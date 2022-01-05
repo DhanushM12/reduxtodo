e6453e5c5d1f8b7bfbc2b3ba9e19a6ac78db8a3e
DhanushM12 <dhanushmukhi@gmail.com>
2022-01-05 21:21:12 +0530

    added components and store

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Taskbar from './Taskbar';
+import Tasklist from './Tasklist';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Taskbar />
+        <Tasklist />
+      </div>
+    )
+  }
+}
+
---
+++ src/components/Taskbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {addTask} from '../actions/index';
+
+export class Taskbar extends Component {
+    render() {
+        return (
+            <div>
+                <input type='text' placeholder='add a new task' ref='task'/>
+                <button onClick={() => this.props.addTask(this.refs.task.value)}>Add Task</button>
+            </div>
+        )
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({addTask}, dispatch);
+}
+
+export default connect(() => {}, mapDispatchToProps)(Taskbar);
